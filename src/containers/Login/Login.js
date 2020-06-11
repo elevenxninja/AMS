54b0c4317b99c3ceed098ea2efdded58d9c10388
RishiSingh1993 <rishingn1@gmail.com>
2020-06-11 14:36:54 +0530

    Login page Name and password alert completed

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -5,6 +5,26 @@ import classes from './Login.css'
 
 
 class Login extends React.Component{
+    state = {
+        userName:'',
+        password:'',
+    }
+
+    changeUserHandler = (e) =>{
+        this.setState({
+            userName: e.target.value,
+        })
+    }
+
+    changePasswordHandler = (e) =>{
+        this.setState({
+            password: e.target.value,
+        })
+    }
+
+    submitHandler = () =>{
+        alert(`Name: ${this.state.userName}, Password: ${this.state.password}`)
+    }
 
     render(){
         return(
@@ -17,14 +37,14 @@ class Login extends React.Component{
                         <div className={classes.User}>
                             <FaUsers size='150px'/>
                             <div className={classes.Input}>
-                                <form>
-                                    <input type='text' placeholder= 'USER NAME'/>
-                                    <input type='password' placeholder= '*******'/>
+                                <form onSubmit={this.submitHandler}>
+                                    <input type='text' placeholder= 'USER NAME' value={this.state.userName} onChange={this.changeUserHandler}/>
+                                    <input type='password' placeholder= '*******' value={this.state.password} onChange={this.changePasswordHandler}/>
                                     <div className={classes.Text}>
                                         <p>Change password</p>
                                         <p>Forgot password</p>
                                     </div>
-                                    <button>LOGIN</button>
+                                    <button type='submit'>LOGIN</button>
                                 </form>
                             </div>
                         </div>
