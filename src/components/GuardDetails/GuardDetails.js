e1ed447a3190b9078095c0c32901c77078fe405a
RishiSingh1993 <rishingn1@gmail.com>
2020-06-22 10:17:33 +0530

    Added id and delete column in guard table

--- src/components/GuardDetails/GuardDetails.js
+++ src/components/GuardDetails/GuardDetails.js
@@ -6,12 +6,18 @@ import classes from './GuardDetails.css';
 const GuardDetails = (props) =>{
     const guardItems = props.guardList.map(list=>{
     return (<tr>
+            <td>{list.id}</td>
             <td onClick={()=>props.clicked(list)}>{list.name}</td>
             <td>{list.mobile}</td>
             <td><ToggleButton
-  value={ props.value || false }
-  onToggle={(value) =>props.toggle(value) } 
-/>
+            value={ props.value || false }
+            onToggle={(value) =>props.toggle(value) } 
+            />
+            </td>
+            <td>
+                <button onClick={props.clickedDelete}>
+                    Delete
+                </button>
             </td>
         </tr>);
     })
@@ -21,9 +27,11 @@ const GuardDetails = (props) =>{
                 <table>
                     <thead>
                         <tr>
+                            <th>Id</th>
                             <th>Name/Username</th>
                             <th>IMEI No.</th>
                             <th>Toggle Button</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -31,6 +39,8 @@ const GuardDetails = (props) =>{
                     </tbody>
                     <tfoot>
                         <tr>
+                            <td></td>
+                            <td></td>
                             <td></td>
                             <td></td>
                             <td></td>
